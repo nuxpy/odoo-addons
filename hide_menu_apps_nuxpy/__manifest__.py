9dbdefae220427076f961b8aa148d12ad59c9672
Félix Urbina <fubina@nuxpy.com>
2025-07-20 13:16:07 +0200

    [ADD] init

---
+++ hide_menu_apps_nuxpy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hide_menu_apps_nuxpy",
+    'summary': "Hide apps menu",
+    'description': """
+Hide apps menu for internal user no admin.
+    """,
+    'author': "nuxpy",
+    'website': "https://www.nuxpy.com",
+    'contributors': [
+        'Félix Urbina <furbina@nuxpy.com>'
+    ],
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/base_menus.xml',
+    ],
+    'license': 'LGPL-3',
+}
