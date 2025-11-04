4230aa5bc5bfd1bb2acbf2e7b72c5a29c4215ad4
Félix Urbina <fubina@nuxpy.com>
2025-11-04 18:52:54 +0100

    [ADD] init version

---
+++ git_odoo_addons_nuxpy/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Git Odoo Addons",
+    'summary': "Get Odoo Addons from Git Repositories",
+    'description': """
+This module allows you to import modules from free repositories like OCA or others via Git.
+    """,
+    'version': '0.1.1',
+    'category': 'setting',
+    'author': "nuxpy",
+    'website': "https://www.nuxpy.com",
+    'contributors': [
+        'Félix Urbina <furbina@nuxpy.com>'
+    ],
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/git_repository_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'support': 'soporte@nuxpy.com',
+    'images': [
+        'static/description/banner.png',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
