c9013e0998b7fdedf81e3756e224a9879eedd779
Félix Urbina <fubina@nuxpy.com>
2025-11-07 00:21:49 +0100

    [IMP] changing position of addons_path variable when is creating a new repository

--- git_odoo_addons_nuxpy/models/git_repository.py
+++ git_odoo_addons_nuxpy/models/git_repository.py
@@ -44,9 +44,10 @@ class GitRepository(models.Model):
             if not os.path.exists(repo_path):
                 os.mkdir(repo_path, mode=0o755)
                 os.system('git clone -b {} {} {}'.format(branch, url, repo_path))
-                vals['addons_path'] = '{}'.format(repo_path)
             else:
                 os.system('cd {}; git pull'.format(repo_path))
+            
+            vals['addons_path'] = '{}'.format(repo_path)
 
         return super(GitRepository, self).create(vals_list)
 
