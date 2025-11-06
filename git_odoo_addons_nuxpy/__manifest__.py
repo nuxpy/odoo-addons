9b2e7573418d8d33770214bd12fc2e1dc72cda8a
Félix Urbina <fubina@nuxpy.com>
2025-11-07 00:19:06 +0100

    [IMP] changing position of addons_path variable when is creating a new repository

--- git_odoo_addons_nuxpy/__manifest__.py
+++ git_odoo_addons_nuxpy/__manifest__.py
@@ -5,7 +5,7 @@
     'description': """
 This module allows you to import modules from free repositories like OCA or others via Git.
     """,
-    'version': '0.1.1',
+    'version': '0.1.2',
     'category': 'setting',
     'author': "nuxpy",
     'website': "https://www.nuxpy.com",
